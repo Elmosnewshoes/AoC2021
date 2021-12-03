106a56abac8abe2ec2f754d0af43fe0ea1c179c6
Bram Durieux <durieux.bram@gmail.com>
2021-12-03 09:02:43 +0100

    Day 3

---
+++ d3.py
@@ -0,0 +1,45 @@
+from pzzl import pzzl
+import numpy as np
+
+data = pzzl(3, False).strings()
+binaries = np.matrix([[int(x) for x in y] for y in data])
+
+length, cols = binaries.shape
+
+ones = binaries.sum(axis = 0)
+
+gamma = ''.join(['1' if (x>length/2) else '0' for x in ones.flat])
+epsilon = ''.join(['0' if (x>length/2) else '1' for x in ones.flat])
+
+
+print(f'ans: gamma * epsilon = {int(gamma,2) * int(epsilon,2)}')
+
+def count(lst, col):
+    running_count = 0
+    for rw in lst:
+        if rw[col] == '1':
+            running_count += 1
+
+    if running_count >= len(lst)/2:
+        return 1
+    return 0
+
+def scrub(lst, crit, col):
+    return [rw for rw in lst if rw[col] == str(crit)]
+
+oxy = data
+co2 = data
+col = 0
+while len(oxy) > 1:
+    comm_val = count(oxy, col)
+    oxy = scrub(oxy, comm_val, col)
+    col += 1
+
+col = 0
+while len(co2) > 1:
+    comm_val = count(co2, col)
+    co2 = scrub(co2, 1- comm_val, col)
+    col += 1
+
+
+print(f'Ans pt2 = {int(oxy[0],2) * int(co2[0],2)}')
