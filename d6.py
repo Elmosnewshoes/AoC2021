450c26ceaa3064728caa38480bbe49e182842ce5
Bram Durieux <durieux.bram@gmail.com>
2022-12-06 11:43:07 +0100

    Refactor function naming

--- d6.py
+++ d6.py
@@ -5,7 +5,7 @@ q6 = pzzl(6, ).strings()[0]
 
 print(tst)
 
-def are_different(txt, i = 0):
+def are_all_characters_unique(txt, i = 0):
     # ugly recursive function
     # start at the first char of the substring
     # if it is unique in the whole substring
@@ -19,19 +19,19 @@ def are_different(txt, i = 0):
     if txt.count(subj) > 1:
         return False
     else:
-        return are_different(txt, i+1)
+        return are_all_characters_unique(txt, i+1)
 
-def find_four_different_characters(txt, n = 4):
+def find_unique_substring(txt, n = 4):
     # execute the subroutine to on each substring
     # to find the first that hass no repeating characters
     for i in range(n, len(txt)):
-        if are_different(txt[i-n:i], 0 ):
+        if are_all_characters_unique(txt[i-n:i], 0 ):
             return i
 
 for t in tst:
     print(t)
-    print(find_four_different_characters(t, 14))
+    print(find_unique_substring(t, 14))
 
-print(find_four_different_characters(q6))
-print(find_four_different_characters(q6, 14))
+print(find_unique_substring(q6))
+print(find_unique_substring(q6, 14))
 
